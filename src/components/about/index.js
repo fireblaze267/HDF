80f3021ec68b0e2b6c877a2c09d07c518ccbdc6d
Vinicius Gama Sá <31103259+fireblaze267@users.noreply.github.com>
2023-02-09 16:32:51 -0300

    feat:styles improvements && style fix

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,61 +1,88 @@
 import React from "react";
-import './styled.css'
+import "./styled.css";
 //icon
-import Infoimg from '../../assets/1617095492522.jpg'
-import nodejs from '../../assets/icons8-nodejs.svg'
-import react from '../../assets/icons8-reagir.svg'
-import postgreesql from '../../assets/icons8-postgreesql.svg'
-import github from '../../assets/icons8-github.svg'
-import javascript from '../../assets/icons8-javascript.svg'
-import angular from '../../assets/icons8-angularjs.svg'
-import linkedin from '../../assets/icons8-linkedin.svg'
+import Infoimg from "../../assets/1617095492522.jpg";
+import nodejs from "../../assets/icons8-nodejs.svg";
+import react from "../../assets/icons8-reagir.svg";
+import postgreesql from "../../assets/icons8-postgreesql.svg";
+import github from "../../assets/icons8-github.svg";
+import javascript from "../../assets/icons8-javascript.svg";
+import angular from "../../assets/icons8-angularjs.svg";
+import linkedin from "../../assets/icons8-linkedin.svg";
+import resume from "../../assets/icons8-resume.png";
 
+function About() {
+  const technologies = [
+    {
+      img: nodejs,
+      alt: "Node.js",
+    },
+    {
+      img: react,
+      alt: "React.js",
+    },
+    {
+      img: github,
+      alt: "github",
+    },
+    {
+      img: javascript,
+      alt: "javascript",
+    },
+    {
+      img: angular,
+      alt: "angular.js",
+    },
+    {
+      img: postgreesql,
+      alt: "postgreesql",
+    },
+  ];
 
-
-
-
-
-
-
-
-function About(){
-    return(
+  return (
     <div className="modal-about">
-        <div class="circular--landscape">
-            <img src={Infoimg} alt="Creatorimage"/>
-       </div>
+      <div class="circular--landscape">
+        <img src={Infoimg} alt="Creatorimage" />
+      </div>
 
-       <div className="info-text">
-            <h1>Hi! my Name is Vinicius and i am a Web Developer</h1>
-            <p>My focus is Node.js, React.js and Javascript</p>
-            <p>Recentely im working whith React and 
-            improving my Skills and learning a lot of Javascript</p>
-            <h1>Here the things i know</h1>
-            <div className="know-imgs">
-                <img src={nodejs} alt="Node.js"/>
-                <img src={react} alt="React.js"/>
-                <img src={postgreesql} alt="postgreesql"/>
-                <img src={github} alt="Github"/>
-                <img src={javascript} alt="javascript"/>
-                <img src={angular} alt="Iknowalitle"/>
-            </div>
-            <h1>Access my networks</h1>
-            <div className="link-network">
-                <div className="itens-network">
-                    <a href="https://github.com/fireblaze267"><img src={github} alt="Github"/><p>Github</p></a>
-                </div>
+      <div className="info-text">
+        <h1>Hi! my Name is Vinicius and i am a Web Developer</h1>
+        <p>My focus is Node.js, React.js and Javascript</p>
+        <p>
+          Recentely im working whith React and improving my Skills and learning
+          a lot of Javascript
+        </p>
+        <h1>Here the things i know</h1>
+        <div className="know-imgs">
+          {technologies.map((e, index) => {
+            return <img key={index} src={e.img} alt={e.alt} />;
+          })}
+        </div>
+        <h1>Access my networks</h1>
+        <div className="link-network">
+          <div className="itens-network">
+            <a href="https://github.com/fireblaze267">
+              <img src={github} alt="Github" />
+              <p>Github</p>
+            </a>
+          </div>
+          <div className="itens-network">
+            <a href="https://resume-ten-ruddy.vercel.app/">
+              <img src={resume} alt="Github" />
+              <p>My site</p>
+            </a>
+          </div>
 
-                <div className="itens-network">
-                    <a href="https://www.linkedin.com/in/vinicius-gama/"><img src={linkedin} alt="Github"/><p>linkedin</p></a>
-                </div>
-            </div>
-
-            
-
-       </div>
-
+          <div className="itens-network">
+            <a href="https://www.linkedin.com/in/vinicius-gama/">
+              <img src={linkedin} alt="Github" />
+              <p>linkedin</p>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-    )
+  );
 }
 
-export default About
+export default About;
--- src/components/leftbar/index.js
+++ src/components/leftbar/index.js
@@ -1,34 +1,44 @@
 import React from "react";
-import './styled.css'
+import "./styled.css";
 //icons
-import homeIconWhite from '../../assets/home_white_24dp.svg'
-import homeIconBlack from '../../assets/home_black_24dp.svg'
-import infoIconWhite from '../../assets/info_white_24dp.svg'
-import infoIconBlack from '../../assets/info_black_24dp.svg'
+import homeIconWhite from "../../assets/home_white_24dp.svg";
+import homeIconBlack from "../../assets/home_black_24dp.svg";
+import infoIconWhite from "../../assets/info_white_24dp.svg";
+import infoIconBlack from "../../assets/info_black_24dp.svg";
 
-import MusicIcon from '../../assets/music_note_white_24dp.svg'
+import MusicIcon from "../../assets/music_note_white_24dp.svg";
 
-
-
-function LeftBar({info,setInfo}){
-    const playlistLink = "https://open.spotify.com/playlist/0rXXhdpACJCsvCpCSa513D?si=89e4bb5f0be64c85"
-    return( 
-        <div className="left">
-           
-            <div onClick={()=>setInfo(false)} className={info === true ?  "option-left" : "option-left select"}>
-                <img src={info === true ? homeIconWhite  : homeIconBlack} alt="home icon"/>
-                <h1>Home</h1>
-            </div>
-            <a className="playlist-link" href={playlistLink}><div className="option-left">
-                <img src={MusicIcon} alt="home icon"/>
-                <h1>Playlist</h1>
-            </div></a> 
-            <div onClick={info === true ? ()=>setInfo(false) :()=>setInfo(true) } className={info ? "option-left select" : "option-left "}>
-                <img src={info ? infoIconBlack : infoIconWhite } alt="home icon"/>
-                <h1>Info</h1>
-            </div>  
+function LeftBar({ info, setInfo }) {
+  const playlistLink =
+    "https://open.spotify.com/playlist/0rXXhdpACJCsvCpCSa513D?si=89e4bb5f0be64c85";
+  return (
+    <div className="left">
+      <div
+        onClick={() => setInfo(false)}
+        className={info === true ? "option-left" : "option-left select"}
+      >
+        <img
+          src={info === true ? homeIconWhite : homeIconBlack}
+          alt="home icon"
+        />
+        <h1>Home</h1>
+      </div>
+      <div
+        onClick={info === true ? () => setInfo(false) : () => setInfo(true)}
+        className={info ? "option-left select" : "option-left "}
+      >
+        <img src={info ? infoIconBlack : infoIconWhite} alt="home icon" />
+        <h1>Info</h1>
+      </div>
+      <a className="playlist-link" href={playlistLink}>
+        <div className="option-left">
+          <img src={MusicIcon} alt="home icon" />
+          <h1>Playlist</h1>
+          <label>spotify</label>
         </div>
-    )
+      </a>
+    </div>
+  );
 }
 
-export default LeftBar
+export default LeftBar;
